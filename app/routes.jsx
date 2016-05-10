3307c2f60d06d789d0ab9f6ea5de3b569586f1f8
Jimmy Lin <usmcjimmy@gmail.com>
2016-05-10 16:26:30 -0400

    initial deployment setup

---
+++ app/routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+import App from 'components/app';
+
+
+/*
+ * @param {Redux Store}
+ * We require store as an argument here because we wish to get
+ * state from the store after it has been authenticated.
+ */
+export default () => {
+
+  return (
+    <Route path="/" component={App}>
+
+    </Route>
+  );
+};
