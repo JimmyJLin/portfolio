c424c5d9b5dd7cccdd088d1fb5f617d0fac205ca
Jimmy Lin <usmcjimmy@gmail.com>
2016-05-23 18:26:21 -0400

    started photography section

--- app/components/photography.js
+++ app/components/photography.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class Photography extends Component {
+
   render(){
     return (
       <div id="photography">
---
+++ app/components/photography/api.js
@@ -0,0 +1,9 @@
+flickr
+
+API_KEY='7d09cc024d8da1bdd970c79ba7291eca'
+SESCRET='2e8bc53a6e123886'
+USER_ID='99263061@N07'
+
+https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=7d09cc024d8da1bdd970c79ba7291eca&user_id=99263061@N07
+
+flickr=`https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${API_KEY}&user_id=${USER_ID}`
